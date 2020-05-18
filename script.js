513f2ab500d52ce5581bca9b153d554e292081c3
Erewerere <Erewerere@users.noreply.github.com>
2020-05-18 21:16:08 +0300

    add

---
+++ script.js
@@ -0,0 +1,50 @@
+jQuery.noConflict();
+jQuery(document).ready(function (e) {
+   
+    jQuery('#form').submit(function(e) {
+        e.preventDefault();
+ 
+em = jQuery("#email").val().trim();
+t = jQuery("#text").val().trim();
+if(!em ){
+    jQuery('#err1').css("display", "true");
+    jQuery("#err1").text("Введіть email");
+    return false;
+    }
+ if(t.length <  10){
+    jQuery('#err1').css("display", "true");
+        jQuery("#err1").text("Введіть більше 10 символів!");
+        return false;
+        }
+jQuery('#err1').css({"display": "true", "color": "green"});
+jQuery("#err1").text("Ваше повідомлення відправлено!");
+jQuery("#text").val("") ; 
+});
+jQuery('#form2').submit(function(e){
+    e.preventDefault();
+f = jQuery("#fio").val().trim();
+console.log(jQuery("#fio").val());
+a = jQuery("#adres").val().trim();
+p = jQuery("#phone").val().trim();
+if(!f){
+    jQuery('#err2').css("display", "true");
+    jQuery("#err2").text("Введіть ФІП");
+    return false;
+    }
+if(!a){
+    jQuery('#err2').css("display", "true");
+        jQuery("#err2").text("Введіть адресу");
+        return false;
+        }
+if(!p ){
+    jQuery('#err2').css("display", "true");
+            jQuery("#err2").text("Введіть номер телефону");
+            return false;
+            }
+            jQuery('#err2').css({"display": "true", "color": "green"});
+        jQuery("#err2").text("Ваше замовлення прийнято!");
+        jQuery("#phone").val("") ;    
+        jQuery("#adres").val("") ; 
+        jQuery("#fio").val("") ; 
+});
+});
